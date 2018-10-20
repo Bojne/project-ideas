696282c3853e814a0acd9e7891953ab8b2eeb75a
Bojne <bojne.john@gmail.com>
2018-10-20 10:55:12 -0700

    update pages

---
+++ gatsby-site/src/pages/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => ( <Layout>
+    <h1> Hi!This is interesting ideas in my brain. </h1>  
+    <p> Who am I ? Check out my < a href = "https://bojne.github.io" > personal website < /a></p >
+    <h3>Ideas list:</h3>
+    <ul>
+        <li> Stayinmydorm - Connecting Minerva, Stanford and Berkley students throught overnight stay in dorms.</li> 
+        <li> Connect RH - Connecting Minerva Residential Hall Common room through video stream.</li> 
+        <li> Green Scan - A Computer Vision mobile app that scan my writing on LCD digital writing board.</li> 
+        <li> Street Art SF - A website collecting SF street art.</li>  
+        <li> Calltheplay - A website that put project commitment on Blockchain</li>
+        <li> Wall of Freinds - Export your facebook friends to a poster</li>
+        <li> Booklized - Turn online article into a book.</li>
+    </ul> 
+    <p> Email me if you are interested to work on these ideas together, or if you got any comments! </p>
+    <p> yhhuang@minerva.kgi.edu </p>
+
+    <Link to = "/page-2/" > Go to page 2 </Link> 
+    </Layout >
+)
+
+export default IndexPage
