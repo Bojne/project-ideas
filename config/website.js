4875a605c062319003151c5b5635f9f07817bfa8
Bojne <bojne.john@gmail.com>
2018-10-20 18:24:47 -0700

    update website config

--- config/website.js
+++ config/website.js
@@ -3,12 +3,12 @@ const tailwind = require('../tailwind');
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-cara.netlify.com', // Domain of your site. No trailing slash!
+  siteTitle: 'Ideas by Yueh Han', // Navigation and Site Title
+  siteTitleAlt: 'Yueh Han', // Alternative Site title for SEO
+  siteUrl: 'https://myideas.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
+  siteDescription: 'Yueh Han has many ideas in his brain, here is where you could find them.',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@cara', // Twitter Username
